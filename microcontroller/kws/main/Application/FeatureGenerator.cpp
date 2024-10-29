48153e2e3fdb43f27376bd63ac0fa29950dbb9c6
lbalic21 <luka.balic@fer.hr>
2024-10-29 18:00:54 +0100

    hann in feature generation

--- microcontroller/kws/main/Application/FeatureGenerator.cpp
+++ microcontroller/kws/main/Application/FeatureGenerator.cpp
@@ -1,6 +1,53 @@
 #include "FeatureGenerator.hpp"
+#include "dsps_fft2r.h"
+#include "dsps_wind.h"
+#include "math.h"
+
+static const char *TAG = "Feature Generator";
+
+FeatureGenerator::FeatureGenerator(Window* window)
+{
+    ESP_LOGI(TAG, "Creating Feature Generator");
+    this->window = window;
+}
 
 bool FeatureGenerator::generateFeatures(int16_t* audioFrame, int8_t* featureSlice)
 {
+    /*****************************************************************/
+    /*************************** WINDOW ******************************/
+    /*****************************************************************/
+
+
+    for(size_t i = 0; i < WINDOW_SIZE; i++)
+    {
+        int32_t value = audioFrame[i] * window->data[i];
+        printf("%d - data %d * window %d = %ld\n", i, audioFrame[i], window->data[i], (value >> WINDOW_BITS));
+        audioFrame[i] = value >> WINDOW_BITS;
+    }
+
+    
+
+    /*****************************************************************/
+    /*************************** FFT *********************************/
+    /*****************************************************************/
+
+    // Initialize the ESP-DSP library
+    //dsps_fft2r_init_fc32(NULL, CONFIG_DSP_MAX_FFT_SIZE);
+
+
+    /*****************************************************************/
+    /*********************** MEL SPECTRO *****************************/
+    /*****************************************************************/
+
+
+    /*****************************************************************/
+    /********************* LOG MEL SPECTRO ***************************/
+    /*****************************************************************/
+
+
+    /*****************************************************************/
+    /*************************** DCT *********************************/
+    /*****************************************************************/
+
     return true;
 }
--- microcontroller/kws/main/Application/FeatureGenerator.hpp
+++ microcontroller/kws/main/Application/FeatureGenerator.hpp
@@ -1,22 +1,23 @@
-#pragma once
+#ifndef FEATURE_GENERATOR_H
+#define FEATURE_GENERATOR_H
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include <string.h>
 #include "stdbool.h"
+#include "Window.hpp"
 
 class FeatureGenerator
 {
-    private:
-        size_t inputAudioFrameSize;
-        size_t outputFeatureSliceSize;
-
     public:
-        FeatureGenerator(size_t inputAudioFrameSize, size_t outputFeatureSliceSize) : inputAudioFrameSize(inputAudioFrameSize),
-                                                                                      outputFeatureSliceSize(outputFeatureSliceSize) {}
-
+        FeatureGenerator(Window* window);
         bool generateFeatures(int16_t* audioFrame, int8_t* featureSlice);
 
+    private:
+        Window* window;
+        
 
 };
+
+#endif
